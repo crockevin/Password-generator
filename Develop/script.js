843ab9db96f59903e1b3f8d854c8301e20b279e2
Kevin <crochedengren@gmail.com>
2023-09-09 15:52:04 -0700

    Added comments also changed the recursion cause it didnt behave properly under certain circumstances

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,22 @@
 // Assignment code here
+//List of characters
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "1234567890"
 var symbols = "!@#$%^&*()-_+=[]{};:,<>?./|"
-
 function generatePassword() {
   var randomPassword = ""
   passwordLength = prompt("Please choose a number 8-128 as the length of your password")
+  //Checks to see of user cancels the prompt
   if (passwordLength == null) {
     return null
+
   }
-  passwordLength = parseInt(passwordLength)
+  passwordLength = parseInt(passwordLength)// converts passwordLength to int
+  //Checks to see if passwordLength fits in the requirements
   if (!(passwordLength > 7 && passwordLength < 129)) {
     alert("Not a valid input")
-    generatePassword()
+    return generatePassword()//Recursion
   }
 
   var listOfCharacters = ""
@@ -23,20 +26,21 @@ function generatePassword() {
   var hasSymbols = confirm("Do you want your password to have symbols")
   if (hasLowerCase) {
     listOfCharacters += lowerCase
-    randomPassword += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length))
+    randomPassword += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length))//Add one of this type incase the password doesnt generate it
   }
   if (hasUpperCase) {
     listOfCharacters += upperCase
-    randomPassword += upperCase.charAt(Math.floor(Math.random() * upperCase.length))
+    randomPassword += upperCase.charAt(Math.floor(Math.random() * upperCase.length))//Add one of this type incase the password doesnt generate it
   }
   if (hasNumbers) {
     listOfCharacters += numbers
-    randomPassword += numbers.charAt(Math.floor(Math.random() * numbers.length))
+    randomPassword += numbers.charAt(Math.floor(Math.random() * numbers.length))//Add one of this type incase the password doesnt generate it
   }
   if (hasSymbols) {
     listOfCharacters += symbols
-    randomPassword += symbols.charAt(Math.floor(Math.random() * symbols.length))
+    randomPassword += symbols.charAt(Math.floor(Math.random() * symbols.length))//Add one of this type incase the password doesnt generate it
   }
+  //Generates the password
   for (var i = randomPassword.length; i < passwordLength; i++) {
     randomPassword += listOfCharacters.charAt(Math.floor(Math.random() * listOfCharacters.length))
   }
