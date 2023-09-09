48d8d29c5b0f6479b50beef36f58839d249d21d8
Kevin <crochedengren@gmail.com>
2023-09-09 12:03:09 -0700

    Added charaters for password, and added if to check password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment code here
-
-
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = 1234567890
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", ";", ":", ",", "<", ">", "?", ".", "/", "|"]
+function generatePassword() {
+  var passwordLength = prompt("Please choose a number 8-128 as the length of your password")
+  if (!(passwordLength > 7 && passwordLength < 129)) {
+    alert("Not a valid input")
+    generatePassword()
+  } else {
+    alert("Alert")
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
